60186f77fe7643fe0693df373d524c6f7c4ae94b
Nikos <nickkaparinos@gmail.com>
2021-08-13 23:10:49 +0300

    DataLoader progress

---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+Nick Kaparinos
+Titanic - Machine Learning from Disaster
+Kaggle Competition
+"""
+from utilities import *
+from random import seed
+
+# C:\Users\Nikos\Desktop\Nikos\HMMY\Code\Google Landmark Recognition 2021\Dataset
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    IMAGE_SIZE = 250
+
+    # Seeds
+    seed(0)
+    np.random.seed(0)
+    tf.random.set_seed(0)
+
+    # Read labels
+    path = "C:/Users/Nikos/Desktop/Nikos/HMMY/Code/Google Landmark Recognition 2021/Dataset"
+    labels = dict(pd.read_csv(
+        filepath_or_buffer="C:/Users/Nikos/Desktop/Nikos/HMMY/Code/Google Landmark Recognition 2021/Dataset/train.csv").values)
+
+    # Data Loader
+    data_loader = DataLoader(batch_size=64, path=path + '/train', IMAGE_SIZE=IMAGE_SIZE)
+    batch = data_loader.return_data()
+
+    names = [b[0] for b in batch]
+    y = [labels[name[:-4]] for name in names]
+    print('kassa')
+    # y = labels(batch)
+
+    # Execution Time
+    end = time.perf_counter()
+    print(f"\nExecution time = {end - start:.2f} second(s)")
+    fet = 5
--- utilities.py
+++ utilities.py
@@ -15,59 +15,15 @@ from sklearn.model_selection import train_test_split
 import cv2
 import math
 import os
-
-
-# class DataGenerator(Sequence):
-#     def __init__(self, path, list_IDs, data, img_size, img_channel, batch_size):
-#         self.path = path
-#         self.list_IDs = list_IDs
-#         self.data = data
-#         self.img_size = img_size
-#         self.img_channel = img_channel
-#         self.batch_size = batch_size
-#         self.indexes = np.arange(len(self.list_IDs))
-#
-#     def __len__(self):
-#         len_ = int(len(self.list_IDs)/self.batch_size)
-#         if len_*self.batch_size < len(self.list_IDs):
-#             len_ += 1
-#         return len_
-#
-#     def __getitem__(self, index):
-#         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-#         list_IDs_temp = [self.list_IDs[k] for k in indexes]
-#         X, y = self.__data_generation(list_IDs_temp)
-#         return X, y
-#
-#
-#     def __data_generation(self, list_IDs_temp):
-#         X = np.zeros((self.batch_size, self.img_size, self.img_size, self.img_channel))
-#         y = np.zeros((self.batch_size, 1), dtype=int)
-#         for i, ID in enumerate(list_IDs_temp):
-#
-#             image_id = self.data.loc[ID, 'id']
-#             file = image_id+'.jpg'
-#             subpath = '/'.join([char for char in image_id[0:3]])
-#
-# #             print(self.path+subpath+'/'+file)
-#             img = cv2.imread(self.path+subpath+'/'+file)
-# #             print(img)
-#             img = img/255
-#             img = cv2.resize(img, (self.img_size, self.img_size))
-#             X[i, ] = img
-#             if self.path.find('train')>=0:
-#                 y[i, ] = self.data.loc[ID, 'landmark_id']
-#             else:
-#                 y[i, ] = 0
-#         return X, y
-
+import time
 
 
 class DataLoader:
-    def __init__(self, batch_size, path):
+    def __init__(self, batch_size, path, IMAGE_SIZE):
         self.batch_size = batch_size
         self.path = path
         self.file_index = 0
+        self.IMAGE_SIZE = IMAGE_SIZE
 
         # Calculate 3 directory permutations
         # Each epoch the directories must be acces in a random order
@@ -82,8 +38,6 @@ class DataLoader:
         # Permute image list too
         self.current_dir_file_list = list(np.random.permutation(self.current_dir_file_list))
 
-        fet = 5
-
     def calc_permutations(self):
         return [np.random.permutation(self.directories) for _ in range(3)]
 
@@ -91,10 +45,21 @@ class DataLoader:
         pass
 
     def return_data(self):
-        for image in self.current_dir_file_list:
-            img = cv2.imread(self.path + '/' + self.current_dir[0] + '/' + self.current_dir[1] + '/' + self.current_dir[2] + '/' + image)
-            cv2.imshow('image', img)
+        batch = []
+        for image_name in self.current_dir_file_list:
+            # Read image
+            img_array = cv2.imread(
+                self.path + '/' + self.current_dir[0] + '/' + self.current_dir[1] + '/' + self.current_dir[
+                    2] + '/' + image_name)
+            img_array = cv2.resize(img_array, (self.IMAGE_SIZE, self.IMAGE_SIZE))
+
+            cv2.imshow('image_name', img_array)
             cv2.waitKey(0)
 
+            # Append to batch
+            batch.append((image_name, img_array))
 
-        return 5
+            if len(batch) >= self.batch_size:
+                break
+
+        return batch
